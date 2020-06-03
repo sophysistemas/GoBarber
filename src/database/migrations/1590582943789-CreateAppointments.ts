382fbc782bb63551f282464c409d36613c81088c
sophysistemas <wellington@sophysistemas.com>
2020-06-03 10:03:57 -0300

    merged with feature/relacionamento-user-appointment

--- src/database/migrations/1590582943789-CreateAppointments.ts
+++ src/database/migrations/1590582943789-CreateAppointments.ts
@@ -10,7 +10,7 @@ export class CreateAppointments1590582943789 implements MigrationInterface {
           columns: [
             {
               name: 'id',
-              type: 'varchar',
+              type: 'uuid',
               isPrimary: true,
               generationStrategy: 'uuid',
               default: 'uuid_generate_v4()',
--- src/database/migrations/1591105557612-CreateUsers.ts
+++ src/database/migrations/1591105557612-CreateUsers.ts
@@ -9,7 +9,7 @@ export class CreateUsers1591105557612 implements MigrationInterface {
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
