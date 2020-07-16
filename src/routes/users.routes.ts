140c567a151c953431864f9ff7487368f6e1a919
sophysistemas <wellington@sophysistemas.com>
2020-07-16 09:25:02 -0300

    update avatar file

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -3,6 +3,8 @@ import multer from 'multer';
 import uploadConfig from '../config/upload';
 
 import CreateUserService from '../services/CreateUserService';
+import UpdateUserAvatarService from '../services/UpdateuserAvatarService';
+
 import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 
 const usersRouter = Router();
@@ -31,7 +33,21 @@ usersRouter.post('/', async (request, response) =>{
 });
 
 usersRouter.patch('/avatar', ensureAuthenticated, upload.single('avatar'), async (request, response) => {
-  console.log(request.file);
+  try {
+    const updateUserAvatar = new UpdateUserAvatarService();
+
+    const user = await updateUserAvatar.execute({
+      user_id: request.user.id,
+      avatarFilename: request.file.filename,
+    });
+
+    delete user.password;
+
+    return response.json(user);
+
+  } catch (err) {
+    return response.status(400).json({ error: err.message });
+  }
 
 
   return response.json({ ok: true });
