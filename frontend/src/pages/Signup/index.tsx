a438c65aa760996e36242a932857b0a7cde2e8a6
sophysistemas <wellington@sophysistemas.com>
2020-09-16 23:44:59 -0300

    fix: input usability

--- frontend/src/pages/Signup/index.tsx
+++ frontend/src/pages/Signup/index.tsx
@@ -1,28 +1,35 @@
 import React from 'react';
 import { FiArrowLeft, FiMail, FiLock, FiUser } from 'react-icons/fi';
+import { Form } from '@unform/web';
 
 import logoImg from '../../assets/logo.svg'
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 import { Container, Content, Background } from './styles';
 
-const SingUp: React.FC = () => (
-  <Container>
-    <Background />
-    <Content>
-      <img src={logoImg} alt="GoBarber" />
+const SingUp: React.FC = () => {
+  function handleSubmit(data: object): void {
+    console.log(data);
+  }
 
-      <form>
-        <h1>Faça seu cadastro</h1>
-        <Input name="name" icon={FiUser} placeholder="Nome" />
-        <Input name="email" icon={FiMail} placeholder="E-mail" />
-        <Input name="password" icon={FiLock} type="password" placeholder="Senha"/>
-        <Button type="button">Cadastrar</Button>
-      </form>
+  return (
+    <Container>
+      <Background />
+      <Content>
+        <img src={logoImg} alt="GoBarber" />
 
-      <a href="login"><FiArrowLeft />Voltar para logon</a>
-    </Content>
-  </Container>
-);
+        <Form onSubmit={handleSubmit}>
+          <h1>Faça seu cadastro</h1>
+          <Input name="name" icon={FiUser} placeholder="Nome" />
+          <Input name="email" icon={FiMail} placeholder="E-mail" />
+          <Input name="password" icon={FiLock} type="password" placeholder="Senha"/>
+          <Button>Cadastrar</Button>
+        </Form>
+
+        <a href="login"><FiArrowLeft />Voltar para logon</a>
+      </Content>
+    </Container>
+  );
+};
 
 export default SingUp;
