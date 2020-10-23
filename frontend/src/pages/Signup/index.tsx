d942ad3eed23df3f9dcc4f64cf172ea904510c59
sophysistemas <wellington@sophysistemas.com>
2020-10-23 08:19:47 -0300

    feature: criada validacao da SingUp com Yup

--- frontend/src/pages/Signup/index.tsx
+++ frontend/src/pages/Signup/index.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { FiArrowLeft, FiMail, FiLock, FiUser } from 'react-icons/fi';
 import { Form } from '@unform/web';
+import * as Yup from 'yup';
 
 import logoImg from '../../assets/logo.svg'
 import Input from '../../components/Input';
@@ -8,9 +9,25 @@ import Button from '../../components/Button';
 import { Container, Content, Background } from './styles';
 
 const SingUp: React.FC = () => {
-  function handleSubmit(data: object): void {
-    console.log(data);
-  }
+  const handleSubmit = useCallback(async(data: object) => {
+    try {
+      const schema = Yup.object().shape({
+        name: Yup.string().required('Nome obrigatório'),
+        email: Yup.string()
+          .required('Email obrigatório')
+          .email('Digite um email válido'),
+        password: Yup.string().required('Digite a senha'),
+      });
+
+      await schema.validate(data, {
+        abortEarly: false,
+      })
+
+    } catch (error) {
+      console.log(error);
+
+    }
+  }, []);
 
   return (
     <Container>
