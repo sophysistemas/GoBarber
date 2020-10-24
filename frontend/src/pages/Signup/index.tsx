7b98d7dad7d5a488710eb5fd61b8b90e16a2a68e
sophysistemas <wellington@sophysistemas.com>
2020-10-24 14:54:33 -0300

    fix: criada funcao para mostrar erros de validacao no form SingUp

--- frontend/src/pages/Signup/index.tsx
+++ frontend/src/pages/Signup/index.tsx
@@ -1,7 +1,9 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useRef } from 'react';
 import { FiArrowLeft, FiMail, FiLock, FiUser } from 'react-icons/fi';
+import { FormHandles } from '@unform/core';
 import { Form } from '@unform/web';
 import * as Yup from 'yup';
+import getValidationErros from '../../utils/getValdationErrors';
 
 import logoImg from '../../assets/logo.svg'
 import Input from '../../components/Input';
@@ -9,8 +11,12 @@ import Button from '../../components/Button';
 import { Container, Content, Background } from './styles';
 
 const SingUp: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
+
   const handleSubmit = useCallback(async(data: object) => {
     try {
+      formRef.current?.setErrors({});
+
       const schema = Yup.object().shape({
         name: Yup.string().required('Nome obrigatório'),
         email: Yup.string()
@@ -23,9 +29,9 @@ const SingUp: React.FC = () => {
         abortEarly: false,
       })
 
-    } catch (error) {
-      console.log(error);
-
+    } catch (err) {
+      const errors = getValidationErros(err);
+      formRef.current?.setErrors(errors);
     }
   }, []);
 
@@ -33,9 +39,9 @@ const SingUp: React.FC = () => {
     <Container>
       <Background />
       <Content>
-        <img src={logoImg} alt="GoBarber" />
+        <img src={ logoImg } alt="GoBarber" />
 
-        <Form onSubmit={handleSubmit}>
+        <Form ref = { formRef } onSubmit = { handleSubmit }>
           <h1>Faça seu cadastro</h1>
           <Input name="name" icon={FiUser} placeholder="Nome" />
           <Input name="email" icon={FiMail} placeholder="E-mail" />
