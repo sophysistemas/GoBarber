8e9f718fc21dd36ebc2700461fca136c818ab792
sophysistemas <wellington@sophysistemas.com>
2020-12-18 08:50:46 -0300

    instalacao e configuracao do cors

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,5 +1,6 @@
 import 'reflect-metadata';
 import express, { Request, Response, NextFunction } from 'express';
+import cors from 'cors';
 import 'express-async-errors';
 import routes from './routes';
 import './database'; // Importa a conexão com o banco de dados
@@ -8,6 +9,7 @@ import AppError from './errors/AppError';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
@@ -19,8 +21,6 @@ app.use((err: Error, request: Request, response: Response, next:NextFunction) =>
       message: err.message,
     });
   }
-
-  console.log(err);
 
   return response.status(500).json({
     status: 'error',
