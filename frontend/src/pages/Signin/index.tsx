d9e4ff37956278b6c537adcc3cac3a54a92d7821
sophysistemas <wellington@sophysistemas.com>
2020-12-29 14:20:37 -0300

    guardar dados do contexto no storage

--- frontend/src/pages/Signin/index.tsx
+++ frontend/src/pages/Signin/index.tsx
@@ -18,8 +18,8 @@ interface SignInFormData {
 const SingIn: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
 
-  const { signIn } = useContext(AuthContext);
-  //console.log(signIn);
+  const { user, signIn } = useContext(AuthContext);
+  console.log(user);
 
   const handleSubmit = useCallback(async(data: SignInFormData) => {
 
