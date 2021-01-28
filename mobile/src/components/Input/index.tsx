0948d45e02060a71937ff44aa2a6ba1f7c3cda5e
sophysistemas <wellington@sophysistemas.com>
2021-01-28 09:52:06 -0300

    reorganizacao de pastas

---
+++ mobile/src/components/Input/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Container } from './styles';
+
+const Input: React.FC = () => (
+  <Container />
+);
+
+export default Input;
--- mobile/src/pages/signIn/index.tsx
+++ mobile/src/pages/signIn/index.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import { Image } from 'react-native';
-
-import { Container, Title } from './styles';
 import logoImg from '../../assets/logo.png';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+import { Container, Title } from './styles';
 
 const SignIn: React.FC = () => {
   return (
     <Container>
       <Image source = { logoImg } />
       <Title>Faça seu logon</Title>
+      <Input />
+      <Input />
+
+      <Button>Entrar</Button>
     </Container>
   );
 }
